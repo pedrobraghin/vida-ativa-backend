4b12268d73afdc19e2856f7ba6c32ffe5bc8ffd3
Pedro Braghin <braghindev@gmail.com>
2023-06-17 11:31:01 -0300

    v0.6:refactor:  update database connection string

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,8 +2,10 @@ import mongoose from "mongoose";
 
 export default class Database {
   private DB_URL = String(process.env.DB_URL);
+  private DB_PASS = String(process.env.DB_PASS);
 
   constructor() {
+    this.DB_URL = this.DB_URL.replace("<PASS>", this.DB_PASS);
     mongoose.set("strictQuery", true);
     mongoose.createConnection(this.DB_URL);
   }
